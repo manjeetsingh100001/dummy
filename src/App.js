8381b541fb2f0ec72c8f166adf48e31dcebd0371
manjeetsingh100001 <manjeetsingh100001@yahoo.com>
2022-06-07 15:35:01 +0530

    vhjf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          chal ga re  ,kaha hai tu<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
